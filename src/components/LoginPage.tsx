ff8f632fb3e8d48e97b3b2f237ecb235ea3ffa3a
vinod <get2vinodtk@gmail.com>
2024-11-30 22:43:52 +0530

    Trigger login on enter button click

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -21,6 +21,12 @@ function LoginPage() {
     userNameRef.current?.focus();
   }, []);
 
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLFormElement>) => {
+    if (e.key === "Enter") {
+      console.log(`Pressed key is ${e.key}`);
+      handleLoginClick();
+    }
+  };
   const handleLoginClick = async () => {
     try {
       if (!isSigningIn) {
@@ -33,6 +39,7 @@ function LoginPage() {
       }
     } catch (err) {
       setLoginError(true);
+      setIsSigningIn(false);
     }
   };
 
@@ -40,61 +47,6 @@ function LoginPage() {
     <>
       {!userLoggedIn && (
         <div className="flex-1 overflow-auto relative z-10 bg-white">
-          {/* <main className="max-w-4xl mx-auto py-6 px-4 lg:px-8 ">
-            <SettingSection icon={User} title={"Login"}>
-              <div className="flex flex-col items-center mb-6 ">
-                <div className="flex flex-col items-start justify-between py-3">
-                  <label className="text-black" htmlFor="username">
-                    Username
-                  </label>
-                  <input
-                    type="email"
-                    id="username"
-                    autoComplete="off"
-                    className="text-black placeholder-gray-400 rounded-lg pl-4 pr-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    onChange={(e) => setUserName(e.target.value)}
-                    value={userName}
-                    required
-                  />
-                </div>
-                <div className="flex flex-col items-start justify-between py-3">
-                  <label className="text-black" htmlFor="password">
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    id="password"
-                    className="text-black placeholder-gray-400 rounded-lg pl-4 pr-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    onChange={(e) => setPassword(e.target.value)}
-                    value={password}
-                    required
-                  />
-                </div>
-                <div className="mt-5 flex justify-between items-start">
-                  <button
-                    className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded transition duration-200 w-full sm:w-auto"
-                    onClick={handleLoginClick}
-                  >
-                    Login
-                  </button>
-                </div>
-                <div className="mt-5 flex justify-between items-start">
-                  <div>
-                    <a href="#" className="text-indigo-800 font-semibold">
-                      Forgot Password?
-                    </a>
-                  </div>
-                </div>
-                {loginError && (
-                  <div className="mt-5 flex justify-between items-start">
-                    <div className="text-red-800 font-semibold">
-                      Login failed. Wrong email or password.
-                    </div>
-                  </div>
-                )}
-              </div>
-            </SettingSection>
-          </main> */}
           <section className="bg-zinc-50">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
               <img
@@ -110,7 +62,10 @@ function LoginPage() {
                   <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                     Sign in to your account
                   </h1>
-                  <form className="space-y-4 md:space-y-6" action="#">
+                  <form
+                    className="space-y-4 md:space-y-6"
+                    onKeyDown={handleKeyDown}
+                  >
                     <div>
                       <label
                         htmlFor="username"
@@ -160,6 +115,13 @@ function LoginPage() {
                     >
                       Sign in
                     </button>
+                    {loginError && (
+                      <div className="mt-5 flex justify-between items-start">
+                        <div className="text-red-600 font-semibold">
+                          Login failed. Wrong email or password.
+                        </div>
+                      </div>
+                    )}
                   </form>
                 </div>
               </div>
