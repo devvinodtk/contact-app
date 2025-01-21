69a0156d266c311a3f94e1038e1b50f638883a89
vinod <get2vinodtk@gmail.com>
2025-01-21 20:36:31 +0530

    Membership ID card view

--- src/components/MembershipCard.tsx
+++ src/components/MembershipCard.tsx
@@ -1,7 +1,10 @@
 import { Download } from "lucide-react";
 import Header from "./common/Header";
+import { useSelector } from "react-redux";
+import { selectActiveMembers } from "../store/MemberSelector";
 
 function MembershipCard() {
+  const activeMembers = useSelector(selectActiveMembers);
   return (
     <>
       <div className="flex-1 overflow-auto relative z-10 mt-16 sm:mt-0">
@@ -13,168 +16,67 @@ function MembershipCard() {
                 <h2 className="mb-2 text-xl ml-10 sm:ml-0 font-semibold text-black flex justify-left items-center">
                   <span>Download Membership Card</span>
                   <a href="#" className="text-red-500">
-                    <Download className="h-6 w-6 ml-2 mt-2 text-red-500" /></a>
+                    <Download className="h-6 w-6 ml-2 mt-2 text-red-500" />
+                  </a>
                 </h2>
 
-
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                  <div className="bg-white shadow-sm rounded-lg border border-gray-200 p-6">
-                    {/* Logo */}
-                    <div className="flex justify-center mb-3">
-                      <img
-                        className=" h-16 mr-2"
-                        src="/assets/Logo-full.png"
-                        alt="Kalakairali"
-                      />
-                    </div>
+                  {activeMembers?.length > 0 &&
+                    activeMembers
+                      .filter(
+                        (activeMember) =>
+                          activeMember.verified && activeMember.displayId !== ""
+                      )
+                      .map((member) => {
+                        return (
+                          <div
+                            key={member.memberId}
+                            className="bg-white shadow-sm rounded-lg border border-gray-200 p-6"
+                          >
+                            {/* Logo */}
+                            <div className="flex justify-center mb-3">
+                              <img
+                                className=" h-16 mr-2"
+                                src="/assets/Logo-full.png"
+                                alt="Kalakairali"
+                              />
+                            </div>
 
-                    {/* Card Content */}
-                    <div className="text-center">
-                      <h2 className="text-xl font-semibold text-gray-800">Suresh Kumar P R</h2>
-                      <p className="text-gray-600  font-semibold">Member ID: KK20250001</p>
-                      <div className="my-2">
-                        <p className="text-gray-500 text-sm">Blood Group: O +ve</p>
-                      </div>
+                            {/* Card Content */}
+                            <div className="text-center">
+                              <h2 className="text-xl font-semibold text-gray-800">
+                                {member.personalDetails.name}
+                              </h2>
+                              <p className="text-gray-600  font-semibold">
+                                Member ID: {member.displayId}
+                              </p>
+                              <div className="my-2">
+                                <p className="text-gray-500 text-sm">
+                                  Blood Group:{" "}
+                                  {member.personalDetails.bloodGroup}
+                                </p>
+                              </div>
 
-                      <div className="flex justify-between text-sm text-gray-500">
-                        <span>Mobile: 8792527628</span>
-                        <span>Location: Sanjayngar</span>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="bg-white shadow-sm rounded-lg border border-gray-200 p-6">
-                    {/* Logo */}
-                    <div className="flex justify-center mb-3">
-                      <img
-                        className=" h-16 mr-2"
-                        src="/assets/Logo-full.png"
-                        alt="Kalakairali"
-                      />
-                    </div>
-
-                    {/* Card Content */}
-                    <div className="text-center">
-                      <h2 className="text-xl font-semibold text-gray-800">Suresh Kumar P R</h2>
-                      <p className="text-gray-600  font-semibold">Member ID: 12345</p>
-                      <div className="my-2">
-                        <p className="text-gray-500 text-sm">Blood Group: O +ve</p>
-                      </div>
-
-                      <div className="flex justify-between text-sm text-gray-500">
-                        <span>Mobile: 8792527628</span>
-                        <span>Location: Sanjayngar</span>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="bg-white shadow-sm rounded-lg border border-gray-200 p-6">
-                    {/* Logo */}
-                    <div className="flex justify-center mb-3">
-                      <img
-                        className=" h-16 mr-2"
-                        src="/assets/Logo-full.png"
-                        alt="Kalakairali"
-                      />
-                    </div>
-
-                    {/* Card Content */}
-                    <div className="text-center">
-                      <h2 className="text-xl font-semibold text-gray-800">Suresh Kumar P R</h2>
-                      <p className="text-gray-600  font-semibold">Member ID: 12345</p>
-                      <div className="my-2">
-                        <p className="text-gray-500 text-sm">Blood Group: O +ve</p>
-                      </div>
-
-                      <div className="flex justify-between text-sm text-gray-500">
-                        <span>Mobile: 8792527628</span>
-                        <span>Location: Sanjayngar</span>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="bg-white shadow-sm rounded-lg border border-gray-200 p-6">
-                    {/* Logo */}
-                    <div className="flex justify-center mb-3">
-                      <img
-                        className=" h-16 mr-2"
-                        src="/assets/Logo-full.png"
-                        alt="Kalakairali"
-                      />
-                    </div>
-
-                    {/* Card Content */}
-                    <div className="text-center">
-                      <h2 className="text-xl font-semibold text-gray-800">Suresh Kumar P R</h2>
-                      <p className="text-gray-600  font-semibold">Member ID: 12345</p>
-                      <div className="my-2">
-                        <p className="text-gray-500 text-sm">Blood Group: O +ve</p>
-                      </div>
-
-                      <div className="flex justify-between text-sm text-gray-500">
-                        <span>Mobile: 8792527628</span>
-                        <span>Location: Sanjayngar</span>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="bg-white shadow-sm rounded-lg border border-gray-200 p-6">
-                    {/* Logo */}
-                    <div className="flex justify-center mb-3">
-                      <img
-                        className=" h-16 mr-2"
-                        src="/assets/Logo-full.png"
-                        alt="Kalakairali"
-                      />
-                    </div>
-
-                    {/* Card Content */}
-                    <div className="text-center">
-                      <h2 className="text-xl font-semibold text-gray-800">Suresh Kumar P R</h2>
-                      <p className="text-gray-600  font-semibold">Member ID: 12345</p>
-                      <div className="my-2">
-                        <p className="text-gray-500 text-sm">Blood Group: O +ve</p>
-                      </div>
-
-                      <div className="flex justify-between text-sm text-gray-500">
-                        <span>Mobile: 8792527628</span>
-                        <span>Location: Sanjayngar</span>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="bg-white shadow-sm rounded-lg border border-gray-200 p-6">
-                    {/* Logo */}
-                    <div className="flex justify-center mb-3">
-                      <img
-                        className=" h-16 mr-2"
-                        src="/assets/Logo-full.png"
-                        alt="Kalakairali"
-                      />
-                    </div>
-
-                    {/* Card Content */}
-                    <div className="text-center">
-                      <h2 className="text-xl font-semibold text-gray-800">Suresh Kumar P R</h2>
-                      <p className="text-gray-600  font-semibold">Member ID: 12345</p>
-                      <div className="my-2">
-                        <p className="text-gray-500 text-sm">Blood Group: O +ve</p>
-                      </div>
-
-                      <div className="flex justify-between text-sm text-gray-500">
-                        <span>Mobile: 8792527628</span>
-                        <span>Location: Sanjayngar</span>
-                      </div>
-                    </div>
-                  </div>
-
+                              <div className="flex justify-between text-sm text-gray-500">
+                                <span>
+                                  Mobile: {member.personalDetails.mobileNumber}
+                                </span>
+                                <span>
+                                  Location: {member.presentAddress.postOffice}
+                                </span>
+                              </div>
+                            </div>
+                          </div>
+                        );
+                      })}
                 </div>
               </div>
-
             </div>
           </div>
-
         </div>
       </div>
-    </>)
+    </>
+  );
 }
 
 export default MembershipCard;
