94d60805c7ec3d206598ff52324685e222e8edc0
Prskumar@123 <sureshkumarprs@gmail.com>
2025-01-26 15:46:30 +0530

    Fixed

--- src/components/AddressList.tsx
+++ src/components/AddressList.tsx
@@ -2,25 +2,87 @@ import { Download } from "lucide-react";
 import Header from "./common/Header";
 import { useSelector } from "react-redux";
 import { selectActiveMembers } from "../store/MemberSelector";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+import React from "react";
 
 const AddressList = () => {
+  const postalAddressRef = React.useRef(null);
+  const namePhoneRef = React.useRef(null);
+
+  const downloadPostalAddress = async () => {
+    const element = postalAddressRef.current;
+    if (!element) {
+      return;
+    }
+
+    const canvas = await html2canvas(element, {
+      scale: 2,
+    });
+    const data = canvas.toDataURL("image/png");
+
+    const pdf = new jsPDF({
+      orientation: "portrait",
+      unit: "px",
+      format: "a4",
+    });
+
+    const imgProperties = pdf.getImageProperties(data);
+    let pdfWidth = pdf.internal.pageSize.getWidth();
+    const margin = 20;
+    pdfWidth = pdfWidth - 2 * margin;
+    const pdfHeight = (imgProperties.height * pdfWidth) / imgProperties.width;
+
+    pdf.addImage(data, "PNG", margin, margin, pdfWidth, pdfHeight);
+    pdf.save("Address List.pdf");
+  }
+  const downloadNamePhoneList = async () => {
+    const element = namePhoneRef.current;
+    if (!element) {
+      return;
+    }
+
+    const canvas = await html2canvas(element, {
+      scale: 2,
+    });
+    const data = canvas.toDataURL("image/png");
+
+    const pdf = new jsPDF({
+      orientation: "portrait",
+      unit: "px",
+      format: "a4",
+    });
+
+    const imgProperties = pdf.getImageProperties(data);
+    let pdfWidth = pdf.internal.pageSize.getWidth();
+    const margin = 20;
+    pdfWidth = pdfWidth - 2 * margin;
+    const pdfHeight = (imgProperties.height * pdfWidth) / imgProperties.width;
+
+    pdf.addImage(data, "PNG", margin, margin, pdfWidth, pdfHeight);
+    pdf.save("Name-Phone-Area.pdf");
+  }
   // return <Header title="Address List" />;
   const activeMembers = useSelector(selectActiveMembers);
+
   return (
     <>
       <div className="flex-1 overflow-auto relative z-10 mt-16 sm:mt-0">
         <Header title="Address List" />
-        <div className="m-5">
-          <div className="w-full mx-auto border rounded-lg mb-6">
-            <div className="overflow-x-auto bg-white text-gray-700  rounded-lg">
+        <div className="w-full rounded-lg pl-6" >
+          <h2 className="text-xl ml-10 p-6 pb-0 sm:ml-0 font-semibold text-black flex justify-left items-center">
+            <span>Postal Address List</span>
+            <span className="text-red-500 cursor-pointer" onClick={downloadPostalAddress}>
+              <Download className="h-6 w-6 ml-2 mt-2 text-red-500" />
+            </span>
+          </h2>
+        </div>
+        <div className="m-5 mt-0">
+          <div className="w-full rounded-lg mb-6" >
+            <div className="overflow-x-auto bg-white text-gray-700  rounded-lg" ref={postalAddressRef}>
               <div className="max-w-full p-6">
-                <h2 className="mb-4 text-xl ml-10 sm:ml-0 font-semibold text-black flex justify-left items-center">
-                  <span>Postal Address List</span>
-                  <a href="#" className="text-red-500">
-                    <Download className="h-6 w-6 ml-2 mt-2 text-red-500" />
-                  </a>
-                </h2>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5" >
                   {activeMembers?.length > 0 &&
                     activeMembers
                       .filter(
@@ -31,26 +93,26 @@ const AddressList = () => {
                         return (
                           <div
                             key={member.memberId}
-                            className="p-4 bg-white border border-gray-200"
+                            className="p-4 bg-white border border-black"
                           >
-                            <p className="text-gray-600 mb-1">To,</p>
-                            <h3 className="text-lg font-semibold text-gray-800">
+                            <p className="text-black mb-1">To,</p>
+                            <h3 className="text-lg font-semibold text-black">
                               {member.personalDetails.name}
                             </h3>
-                            <p className="text-gray-600">
+                            <p className="text-black">
                               {member.presentAddress.flatNumberName}
                             </p>
-                            <p className="text-gray-600">
+                            <p className="text-black">
                               {member.presentAddress.addressLine1},{" "}
                               {member.presentAddress.addressLine2}
                             </p>
-                            <p className="text-gray-600">
+                            <p className="text-black">
                               {member.presentAddress.city},{" "}
                               {member.presentAddress.state} -{" "}
                               {member.presentAddress.pincode}{" "}
                             </p>
-                            <p className="text-gray-600">
-                              Phone: {"+91 - "}{" "}
+                            <p className="text-black">
+                              Phone: {"+91 "}{" "}
                               {member.personalDetails.mobileNumber}
                             </p>
                           </div>
@@ -61,77 +123,30 @@ const AddressList = () => {
             </div>
           </div>
 
-          <div className="w-full mx-auto border rounded-lg mb-6">
-            <div className="overflow-x-auto bg-white text-gray-700  rounded-lg">
-              <div className="max-w-full p-6">
-                <h2 className="mb-4 text-xl ml-10 sm:ml-0 font-semibold text-black flex justify-left items-center">
-                  <span>Address List - Dierect</span>
-                  <a href="#" className="text-red-500">
-                    <Download className="h-6 w-6 ml-2 mt-2 text-red-500" />
-                  </a>
-                </h2>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-                  {activeMembers?.length > 0 &&
-                    activeMembers
-                      .filter(
-                        (member) =>
-                          member.communicationPreference === "In Person"
-                      )
-                      .map((member) => {
-                        return (
-                          <div
-                            key={member.memberId}
-                            className="p-4 bg-white border border-gray-200"
-                          >
-                            <p className="text-gray-600 mb-1">To,</p>
-                            <h3 className="text-lg font-semibold text-gray-800">
-                              {member.personalDetails.name}
-                            </h3>
-                            <p className="text-gray-600">
-                              {member.presentAddress.flatNumberName}
-                            </p>
-                            <p className="text-gray-600">
-                              {member.presentAddress.addressLine1},{" "}
-                              {member.presentAddress.addressLine2}
-                            </p>
-                            <p className="text-gray-600">
-                              {member.presentAddress.city},{" "}
-                              {member.presentAddress.state} -{" "}
-                              {member.presentAddress.pincode}{" "}
-                            </p>
-                            <p className="text-gray-600">
-                              Phone: {"+91 -"}{" "}
-                              {member.personalDetails.mobileNumber}
-                            </p>
-                          </div>
-                        );
-                      })}
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="max-w-4xl mx-auto p-4 border rounded-lg mb-6">
-            <h2 className="mb-4 text-xl ml-10 sm:ml-0 font-semibold text-black flex justify-left items-center">
+          <div className="w-full rounded-lg pl-6" >
+            <h2 className="text-xl ml-10 sm:ml-0 font-semibold text-black flex justify-left items-center">
               <span>Members list with mobile number</span>
-              <a href="#" className="text-red-500">
+              <span className="text-red-500 cursor-pointer" onClick={downloadNamePhoneList}>
                 <Download className="h-6 w-6 ml-2 mt-2 text-red-500" />
-              </a>
+              </span>
             </h2>
+          </div>
+
+          <div className="w-full mx-auto p-6 mb-6">
             <div className="overflow-x-auto">
-              <table className="w-full border-collapse border border-gray-300">
+              <table className="w-full border-collapse border border-black" ref={namePhoneRef}>
                 <thead className="bg-gray-100">
                   <tr>
-                    <th className="border border-gray-300 px-4 py-2 text-left text-gray-700">
+                    <th className="border border-black px-4 py-2 text-left text-black">
                       Sl. No.
                     </th>
-                    <th className="border border-gray-300 px-4 py-2 text-left text-gray-700">
+                    <th className="border border-black px-4 py-2 text-left text-black">
                       Name
                     </th>
-                    <th className="border border-gray-300 px-4 py-2 text-left text-gray-700">
+                    <th className="border border-black px-4 py-2 text-left text-black">
                       Mobile
                     </th>
-                    <th className="border border-gray-300 px-4 py-2 text-left text-gray-700">
+                    <th className="border border-black px-4 py-2 text-left text-black">
                       Area
                     </th>
                   </tr>
@@ -141,16 +156,16 @@ const AddressList = () => {
                     activeMembers.map((member, index) => {
                       return (
                         <tr key={member.memberId} className="hover:bg-gray-50">
-                          <td className="border border-gray-300 px-4 py-2 text-gray-600">
+                          <td className="border border-black px-4 py-2 text-black">
                             {index + 1}
                           </td>
-                          <td className="border border-gray-300 px-4 py-2 text-gray-600">
+                          <td className="border border-black px-4 py-2 text-black">
                             {member.personalDetails.name}
                           </td>
-                          <td className="border border-gray-300 px-4 py-2 text-gray-600">
+                          <td className="border border-black px-4 py-2 text-black">
                             +91 - {member.personalDetails.mobileNumber}
                           </td>
-                          <td className="border border-gray-300 px-4 py-2 text-gray-600">
+                          <td className="border border-black px-4 py-2 text-black">
                             {member.presentAddress.postOffice}
                           </td>
                         </tr>
--- src/components/MembershipCard.tsx
+++ src/components/MembershipCard.tsx
@@ -2,9 +2,40 @@ import { Download } from "lucide-react";
 import Header from "./common/Header";
 import { useSelector } from "react-redux";
 import { selectActiveMembers } from "../store/MemberSelector";
-
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+import React from "react";
 function MembershipCard() {
   const activeMembers = useSelector(selectActiveMembers);
+  const memberIdRef = React.useRef(null);
+
+  const DownloadMmeberId = async () => {
+    const element = memberIdRef.current;
+    if (!element) {
+      return;
+    }
+
+    const canvas = await html2canvas(element, {
+      scale: 2,
+      useCORS: true
+    });
+    const data = canvas.toDataURL("image/png");
+
+    const pdf = new jsPDF({
+      orientation: "portrait",
+      unit: "px",
+      format: "a4",
+    });
+
+    const imgProperties = pdf.getImageProperties(data);
+    let pdfWidth = pdf.internal.pageSize.getWidth();
+    const margin = 20;
+    pdfWidth = pdfWidth - 2 * margin;
+    const pdfHeight = (imgProperties.height * pdfWidth) / imgProperties.width;
+
+    pdf.addImage(data, "PNG", margin, margin, pdfWidth, pdfHeight);
+    pdf.save("Members-Id-Card.pdf");
+  };
   return (
     <>
       <div className="flex-1 overflow-auto relative z-10 mt-16 sm:mt-0">
@@ -15,12 +46,12 @@ function MembershipCard() {
               <div className="max-w-full p-6">
                 <h2 className="mb-2 text-xl ml-10 sm:ml-0 font-semibold text-black flex justify-left items-center">
                   <span>Download Membership Card</span>
-                  <a href="#" className="text-red-500">
+                  <span onClick={DownloadMmeberId} className="text-red-500 cursor-pointer">
                     <Download className="h-6 w-6 ml-2 mt-2 text-red-500" />
-                  </a>
+                  </span>
                 </h2>
 
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4" ref={memberIdRef}>
                   {activeMembers?.length > 0 &&
                     activeMembers
                       .filter(
@@ -44,27 +75,27 @@ function MembershipCard() {
                             </div>
                             <div className="flex justify-end items-center pt-2 pb-2 mb-2 max-w-md border-b-[3px] border-[#3c3b98]">
                               <div className="text-right mr-4">
-                                <p className="text-xl font-semibold text-gray-800">{member.personalDetails.name}</p>
-                                <p className="text-gray-600  font-semibold">Member ID: {member.displayId}</p>
+                                <p className="text-xl font-semibold text-black">{member.personalDetails.name}</p>
+                                <p className="text-black  font-semibold">Member ID: {member.displayId}</p>
                               </div>
                               <div>
-                              <img
-                                className=" h-20 rounded border-2 border-gray-200"
-                                src={
-                                  member.personalDetails?.profilePhotoUrl
-                                    ? member.personalDetails
+                                <img
+                                  className=" h-20 rounded border-2 border-gray-200"
+                                  src={
+                                    member.personalDetails?.profilePhotoUrl
+                                      ? member.personalDetails
                                         .profilePhotoUrl
-                                    : `/assets/member_${member.personalDetails?.gender.toLocaleLowerCase()}.png`
-                                }
-                                alt="Kalakairali"
-                              />
+                                      : `/assets/member_${member.personalDetails?.gender.toLocaleLowerCase()}.png`
+                                  }
+                                  alt="Kalakairali"
+                                />
                               </div>
-                            </div>                           
+                            </div>
 
                             {/* Card Content */}
-                            <div className="text-center py-2">                       
+                            <div className="text-center py-2">
 
-                              <div className="flex justify-between text-sm text-gray-500">
+                              <div className="flex justify-between text-sm text-black">
                                 <span>
                                   {member.personalDetails.mobileNumber}
                                 </span>
@@ -73,7 +104,7 @@ function MembershipCard() {
                                 </span>
                               </div>
                             </div>
-                            
+
                           </div>
                         );
                       })}
